9f64e0decebffa9be96754d6cbd41875f8cdc4a4
ElijahRich1113 <erichards2508@myhta.org>
2018-09-26 12:47:39 -1000

    part 1-2 done

---
+++ data.js
@@ -0,0 +1,123 @@
+const data = [
+    {
+        artist: "Minnie Riperton",
+        genre: "R&B/Soul",
+        song: "Lovin' You",
+        image: ""
+    }, 
+    {
+        artist: "Boyz II Men",
+        genre: "R&B/Soul",
+        song: "It's So Hard to Say Goodbye to Yesterday",
+        image: ""
+    }, 
+    {
+        artist: "Boyz II Men",
+        genre: "R&B/Soul",
+        song: "A Song For Mama",
+        image: ""
+    }, 
+    {
+        artist: "All-4-One",
+        genre: "R&B/Soul",
+        song: "So Much In Love",
+        image: ""
+    }, 
+    {
+        artist: "All-4-One",
+        genre: "R&B/Soul",
+        song: "I Swear",
+        image: ""
+    }, 
+    {
+        artist: "Sabrina Claudio",
+        genre: "R&B/Soul",
+        song: "Albums - About Time and Confidently Lost",
+        image: ""
+    },
+    {
+        artist: "Sabrina Claudio",
+        genre: "R&B/Soul",
+        song: "Albums - Don't Let Me Down/All to You and No Rain, No Flowers",
+        image: ""
+    }, 
+    {
+        artist: "Childish Gambino",
+        genre: "R&B/Soul",
+        song: "Album - Awaken My Love",
+        image: ""
+    }, 
+    {
+        artist: "Childish Gambino",
+        genre: "R&B/Soul",
+        song: "Album - Summer Pack",
+        image: ""
+    }, 
+    {
+        artist: "The Weeknd",
+        genre: "R&B/Soul",
+        song: "Angel",
+        image: ""
+    }, 
+    //split between genres
+    {
+        artist: "Russ",
+        genre: "Hip Hop/Rap",
+        song: "Ain't Nobody Taking My Baby",
+        image: ""
+    }, 
+    {
+        artist: "GoldLink",
+        genre: "Hip Hop/Rap",
+        song: "Dark Skin Women",
+        image: ""
+    }, 
+    {
+        artist: "Childish Gambino",
+        genre: "Hip Hop/Rap",
+        song: "Sober",
+        image: ""
+    }, 
+    {
+        artist: "Childish Gambino",
+        genre: "Hip Hop/Rap",
+        song: "Bonfire",
+        image: ""
+    }, 
+    {
+        artist: "Childish Gambino",
+        genre: "Hip Hop/Rap",
+        song: "Album - Because the Internet",
+        image: ""
+    }, 
+    {
+        artist: "Nelly",
+        genre: "Hip Hop/Rap",
+        song: "The Fix",
+        image: ""
+    }
+    {
+        artist: "Tyler, The Creator",
+        genre: "Hip Hop/Rap",
+        song: "See You Again",
+        image: ""
+    }, 
+    {
+        artist: "Smino",
+        genre: "Hip Hop/Rap",
+        song: "Album - blkswn",
+        image: ""
+    }, 
+    {
+        artist: "Juice WRLD",
+        genre: "Hip Hop/Rap",
+        song: "Lucid Dreams",
+        image: ""
+    }, 
+    {
+        artist: "J. Cole",
+        genre: "Hip Hop/Rap",
+        song: "Crooked Smile",
+        image: ""
+    }
+];
