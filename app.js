99d8f908faa0affa7e54a56e7eae481c2fc308c5
ElijahRich1113 <erichards2508@myhta.org>
2018-10-02 19:01:04 -1000

    mvp

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log(data);
+const Soul = document.querySelector("#soul-container");
+const hipHop = document.querySelector("#hiphop-container");
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+
+
+btn1.addEventListener("click", function(){
+    const playlist = data.filter(songs => {
+        return songs.genre === 'R&B/Soul';
+      })
+      .map(songs => {
+        return `
+         <h1>R&B/Soul</h1>
+         <div>Cover:${songs.image}</div>
+         <div>Artist:${songs.artist}</div>
+         <div>Song Title:${songs.song}</div>
+        `;
+      })
+      .join('');
+    
+      Soul.innerHTML = playlist;
+});
+
+
+  
+btn2.addEventListener("click", function(){
+    const playlist = data.filter(songs => {
+        return songs.genre === 'Hip Hop/Rap';
+    })
+    .map(songs => {
+        return `
+         <h1>Hip Hop/Rap</h1>
+         <div>Cover:${songs.image}</div>
+         <div>Artist:${songs.artist}</div>
+         <div>Song Title:${songs.song}</div>
+        `;
+      })
+      .join('');
+
+      hipHop.innerHTML = playlist;
+});
--- data.js
+++ data.js
@@ -3,121 +3,121 @@ const data = [
         artist: "Minnie Riperton",
         genre: "R&B/Soul",
         song: "Lovin' You",
-        image: ""
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Perfect_Angel_%C2%A91974.jpg/220px-Perfect_Angel_%C2%A91974.jpg"
     }, 
     {
         artist: "Boyz II Men",
         genre: "R&B/Soul",
         song: "It's So Hard to Say Goodbye to Yesterday",
-        image: ""
+        image: "https://images.genius.com/902fe66baab43333a48dc5027db1c065.1000x1000x1.jpg"
     }, 
     {
         artist: "Boyz II Men",
         genre: "R&B/Soul",
         song: "A Song For Mama",
-        image: ""
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/A_Song_for_Mama_Boyz_II_Men.jpg/220px-A_Song_for_Mama_Boyz_II_Men.jpg"
     }, 
     {
         artist: "All-4-One",
         genre: "R&B/Soul",
         song: "So Much In Love",
-        image: ""
+        image: "https://images-na.ssl-images-amazon.com/images/I/71W%2BCJCI57L._SY355_.jpg"
     }, 
     {
         artist: "All-4-One",
         genre: "R&B/Soul",
         song: "I Swear",
-        image: ""
+        image: "https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2F1e718f8838a66ba2c90fef59a0ca7641.600x600x1.jpg"
     }, 
     {
         artist: "Sabrina Claudio",
         genre: "R&B/Soul",
         song: "Albums - About Time and Confidently Lost",
-        image: ""
+        image: "https://images.genius.com/45eebb3fbb664f0aae3177202577bc06.500x500x1.jpg"
     },
     {
         artist: "Sabrina Claudio",
         genre: "R&B/Soul",
         song: "Albums - Don't Let Me Down/All to You and No Rain, No Flowers",
-        image: ""
+        image: "https://images.complex.com/complex/images/c_limit,w_600/fl_lossy,pg_1,q_auto/tftubd0nfvwh4gc1l52g/sabrina"
     }, 
     {
         artist: "Childish Gambino",
         genre: "R&B/Soul",
         song: "Album - Awaken My Love",
-        image: ""
+        image: "https://images-na.ssl-images-amazon.com/images/I/617vrHEDTRL._SY450_.jpg"
     }, 
     {
         artist: "Childish Gambino",
         genre: "R&B/Soul",
         song: "Album - Summer Pack",
-        image: ""
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/d/de/Childish_Gambino_-_Summer_Pack.jpg/220px-Childish_Gambino_-_Summer_Pack.jpg"
     }, 
     {
         artist: "The Weeknd",
         genre: "R&B/Soul",
         song: "Angel",
-        image: ""
+        image: "https://images.rapgenius.com/3ddb43014218a83d897137b68b212e0a.600x600x1.jpg"
     }, 
     //split between genres
     {
         artist: "Russ",
         genre: "Hip Hop/Rap",
         song: "Ain't Nobody Taking My Baby",
-        image: ""
+        image: "https://sslf.ulximg.com/image/750x750/cover/1482542446_11d918203b10642f7bd06a932429dc6d.jpg/60416113d2131e29565d2693de6bf495/1482542446_afe64ec75b018e152cde161b45c54132.jpg"
     }, 
     {
         artist: "GoldLink",
         genre: "Hip Hop/Rap",
         song: "Dark Skin Women",
-        image: ""
+        image: "https://images.genius.com/57bc8299663cab89d8bca7f0baf60d56.1000x1000x1.jpg"
     }, 
     {
         artist: "Childish Gambino",
         genre: "Hip Hop/Rap",
         song: "Sober",
-        image: ""
+        image: "https://d2tml28x3t0b85.cloudfront.net/tracks/artworks/000/508/797/original/b0aa25.jpeg?1492782862"
     }, 
     {
         artist: "Childish Gambino",
         genre: "Hip Hop/Rap",
         song: "Bonfire",
-        image: ""
+        image: "https://images-na.ssl-images-amazon.com/images/I/81NzJqyL8IL._SL1294_.jpg"
     }, 
     {
         artist: "Childish Gambino",
         genre: "Hip Hop/Rap",
         song: "Album - Because the Internet",
-        image: ""
+        image: "https://is3-ssl.mzstatic.com/image/thumb/Music4/v4/c7/e6/b0/c7e6b060-51c0-971b-0d80-518cd97db3c3/UMG_cvrart_00044003173460_01_RGB72_1502x1502_13CANIM00972.jpg/1200x630bb.jpg"
     }, 
     {
         artist: "Nelly",
         genre: "Hip Hop/Rap",
         song: "The Fix",
-        image: ""
-    }
+        image: "https://sslf.ulximg.com/image/750x750/cover/1438883206_538d5b113b873606cf4d01f330b2eaab.png/667693c8e1a4e219434c2a7ae7480af2/1438883206_4cfb7f9b469fc8387e22623e79bffdd4.png"
+    },
     {
         artist: "Tyler, The Creator",
         genre: "Hip Hop/Rap",
         song: "See You Again",
-        image: ""
+        image: "https://images.genius.com/5e4c5eba4b6e9cd441808dcff5434a3c.1000x1000x1.jpg"
     }, 
     {
         artist: "Smino",
         genre: "Hip Hop/Rap",
         song: "Album - blkswn",
-        image: ""
+        image: "http://images.genius.com/58e4aa8efab906f663fd311dd2ce8201.1000x1000x1.jpg"
     }, 
     {
         artist: "Juice WRLD",
         genre: "Hip Hop/Rap",
         song: "Lucid Dreams",
-        image: ""
+        image: "https://i1.sndcdn.com/artworks-9Fj1bzj0FaWZ-0-t500x500.jpg"
     }, 
     {
         artist: "J. Cole",
         genre: "Hip Hop/Rap",
         song: "Crooked Smile",
-        image: ""
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/9/95/CrookedSmileJCole.jpg/220px-CrookedSmileJCole.jpg"
     }
 ];
