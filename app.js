eb86ec68cc0ed1a463d38799b1d3fdae65936837
ElijahRich1113 <erichards2508@myhta.org>
2018-10-05 11:14:43 -1000

    need help with css

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const Soul = document.querySelector("#soul-container");
-const hipHop = document.querySelector("#hiphop-container");
+const genres = document.querySelector("#container");
 const btn1 = document.querySelector("#btn1");
 const btn2 = document.querySelector("#btn2");
 
@@ -10,32 +9,30 @@ btn1.addEventListener("click", function(){
       })
       .map(songs => {
         return `
-         <h1>R&B/Soul</h1>
-         <div>Cover:${songs.image}</div>
+         <img class = "images" src = "${songs.image}"/>
          <div>Artist:${songs.artist}</div>
          <div>Song Title:${songs.song}</div>
         `;
       })
       .join('');
     
-      Soul.innerHTML = playlist;
+      genres.innerHTML = playlist;
 });
 
 
   
 btn2.addEventListener("click", function(){
-    const playlist = data.filter(songs => {
+    const playlist2 = data.filter(songs => {
         return songs.genre === 'Hip Hop/Rap';
     })
     .map(songs => {
         return `
-         <h1>Hip Hop/Rap</h1>
-         <div>Cover:${songs.image}</div>
+         <img class = "images" src = "${songs.image}"/>
          <div>Artist:${songs.artist}</div>
          <div>Song Title:${songs.song}</div>
         `;
       })
       .join('');
 
-      hipHop.innerHTML = playlist;
+      genres.innerHTML = playlist2;
 });
